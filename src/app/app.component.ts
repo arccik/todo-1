11c8dc35c7216c62b91818b7e74df572d44b2bb3
arccik <arccik@gmail.com>
2020-10-24 14:57:06 +0100

    Angular Todo-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+template: '<h1> Welcome to {{this.title}} </h1> <li *ngFor="let item of [1,2,3,4]; index as i">{{i}}...</li>',
+  styles: ['h1 { color: red}']
 })
 export class AppComponent {
   title = 'Todo';
