7d4ede07aa835ea9f29c5e911a33385b11484503
arccik <arccik@gmail.com>
2020-10-25 08:22:36 +0000

    Transfered template to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-template: '<h1> Welcome to {{this.title}} </h1> <li *ngFor="let item of [1,2,3,4]; index as i">{{i}}...</li>',
-  styles: ['h1 { color: red}']
+templateUrl: 'app.component.html',
+  styles: ['']
 })
 export class AppComponent {
   title = 'Todo';
